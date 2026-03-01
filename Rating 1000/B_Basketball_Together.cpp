6823cb7ebee33c7e35f81ba51343181aba425340
tijulkabir <tijulkabir4@gmail.com>
2026-03-01 14:30:49 +0600

    nice one the player not the div2

---
+++ Rating 1000/B_Basketball_Together.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+static inline void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+using ll = long long;
+using i32 = int;
+using ull = unsigned long long;
+
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template <class F>
+ll bs(ll l, ll r, F f)
+{
+    while (l < r)
+    {
+        ll m = l + (r - l) / 2;
+        if (f(m))
+            r = m;
+        else
+            l = m + 1;
+    }
+    return l;
+}
+
+void solve()
+{
+    ll n, d;
+    cin >> n >> d;
+    vll v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(rall(v));
+    ll cnt = 0, res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ll pl = (d / v[i]) + 1;
+        cnt += pl;
+        if (cnt <= n)
+            res++;
+        else
+        {
+            break;
+        }
+    }
+
+    cout << res << endl;
+}
+
+int main()
+{
+    fastio();
+    solve();
+    return 0;
+}
