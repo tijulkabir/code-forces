468fbf8b792ba03ac1ffbd0a5dffe50052427dea
tijulkabir <tijulkabir4@gmail.com>
2026-03-10 23:41:21 +0600

    just surviving#include <bits/stdc++.h>

---
+++ Rating 1000/C_Double_ended_Strings.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+static inline void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+using ll = long long;
+using i32 = int;
+using ull = unsigned long long;
+
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template <class F>
+ll bs(ll l, ll r, F f)
+{
+    while (l < r)
+    {
+        ll m = l + (r - l) / 2;
+        if (f(m))
+            r = m;
+        else
+            l = m + 1;
+    }
+    return l;
+}
+
+void solve()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    ll n = s1.size();
+    ll m = s2.size();
+    ll bm = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            ll k = 0;
+
+            while (i + k < n && j + k < m && s1[i + k] == s2[j + k])
+            {
+                k++;
+            }
+
+            bm = max(bm, k);
+        }
+    }
+
+    cout << n + m - 2 * bm << "\n";
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
